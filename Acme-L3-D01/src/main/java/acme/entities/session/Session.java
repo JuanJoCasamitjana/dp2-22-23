8c50953636f00065d614cc905f005e6187e4a0c9
meryknows <mariabarrancosm@gmail.com>
2023-03-16 13:28:33 +0100

    entities and role

---
+++ Acme-L3-D01/src/main/java/acme/entities/session/Session.java
@@ -0,0 +1,42 @@
+
+package acme.entities.session;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.tutorial.Tutorial;
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Session extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	protected static final long	serialVersionUID	= 1L;
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+	@NotBlank
+	@Length(max = 101)
+	protected String			abstractMessage;
+	protected boolean			isTheoretical;
+	@Min(1)
+	@Max(5)
+	protected double			periodTime;
+	@URL
+	protected String			optionalUrl;
+	@ManyToOne
+	protected Tutorial			tutorial;
+
+}
---
+++ Acme-L3-D01/src/main/java/acme/entities/tutorial/Tutorial.java
@@ -0,0 +1,44 @@
+
+package acme.entities.tutorial;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.entities.session.Session;
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Tutorial extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	protected static final long	serialVersionUID	= 1L;
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3} [0-9]{3}")
+	protected String			code;
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+	@NotBlank
+	@Length(max = 101)
+	protected String			abstractMessage;
+	@NotBlank
+	@Length(max = 101)
+	protected String			goals;
+	protected Double			estimatedTime;
+	@ManyToOne
+	protected Session			session;
+
+}
