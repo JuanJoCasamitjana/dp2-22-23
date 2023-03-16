06ab1135ace584fd197494cd8cfe3baf3456ed73
davgonher1 <72940888+gahijil@users.noreply.github.com>
2023-03-16 11:00:15 +0100

    Creacion de AuditorDashboard

---
+++ Acme-L3-D01/src/main/java/acme/forms/AuditorDashboard.java
@@ -0,0 +1,27 @@
+
+package acme.forms;
+
+import acme.framework.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuditorDashboard extends AbstractForm {
+
+	//REVISAR
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+	int							theoryCourses;
+	int							handsOnCourses;
+	double						averageNumberOfAuditingRecords;
+	double						deviationNumberOfAuditingRecords;
+	double						minimumNumberOfAuditingRecords;
+	double						maximumNumberOfAuditingRecords;
+	double						averageTimeOfPeriodLengths;
+	double						deviationTimeOfPeriodLengths;
+	double						minimumTimeOfPeriodLengths;
+	double						maximumTimeOfPeriodLengths;
+}
