9c6416c9ae0975fc932ce342d50591f661fbff62
Alejandro Ortiz Pagador <48619510+aleortpag@users.noreply.github.com>
2023-03-17 20:13:47 +0100

    Lo de David

---
+++ Acme-L3-D01/src/main/java/acme/entities/note/Note.java
@@ -0,0 +1,50 @@
+
+package acme.entities.note;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class Note extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@PastOrPresent()
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				instantiation;
+
+	@NotBlank
+	@Size(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Size(max = 75)
+	protected String			author;
+
+	@NotBlank
+	@Size(max = 100)
+	protected String			message;
+
+	@Email
+	protected String			mail;
+
+	@URL
+	protected String			link;
+
+}
